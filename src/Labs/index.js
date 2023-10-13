abaa12504fb32cc0847bee7bb19313df2640f129
kevinpark30 <cykevin30@gmail.com>
2023-10-12 23:16:32 -0400

    Finished lab portion of a3

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,10 +1,12 @@
 import Assignment3 from "./a3";
+import Nav from "../Nav";
 
 function Labs() {
- return (
-   <div className="container">
-     <Assignment3/>
-   </div>
- );
+    return (
+        <div>
+            <Nav />
+            <Assignment3 />
+        </div>
+    );
 }
 export default Labs;
