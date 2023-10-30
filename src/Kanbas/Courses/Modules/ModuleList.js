c667444f091fe21efe512edb83a3ce64a7a5d864
kevinpark30 <cykevin30@gmail.com>
2023-10-30 18:50:02 -0400

    Finished Kanbas portion of a4

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import db from "../../Database";
-import "./ModuleList.css";
 import { FaGripVertical } from "react-icons/fa6";
 import { IconContext } from "react-icons";
+import { useSelector, useDispatch } from "react-redux";
+import {
+    addModule,
+    deleteModule,
+    updateModule,
+    setModule,
+} from "./modulesReducer";
+import "./ModuleList.css";
 
 function ModuleList() {
     const { courseId } = useParams();
-    const modules = db.modules;
+    const modules = useSelector((state) => state.modulesReducer.modules);
+    const module = useSelector((state) => state.modulesReducer.module);
+    const dispatch = useDispatch();
 
     const icons = [
         <IconContext.Provider value={{ color: "grey", size: 28 }}>
@@ -16,10 +25,65 @@ function ModuleList() {
     ];
     return (
         <ul className="list-group module-list">
+            <li className="list-group-item grey">
+                <button
+                    className="btn green mx-3"
+                    onClick={() =>
+                        dispatch(addModule({ ...module, course: courseId }))
+                    }
+                >
+                    Add
+                </button>
+                <button
+                    className="btn red mx-3"
+                    onClick={() => dispatch(updateModule(module))}
+                >
+                    Update
+                </button>
+                <div>
+                    <input
+                        className="mx-3"
+                        value={module.name}
+                        onChange={(e) =>
+                            dispatch(
+                                setModule({ ...module, name: e.target.value })
+                            )
+                        }
+                    />
+
+                    <textarea
+                        className="mt-4"
+                        value={module.description}
+                        onChange={(e) =>
+                            dispatch(
+                                setModule({
+                                    ...module,
+                                    description: e.target.value,
+                                })
+                            )
+                        }
+                    />
+                </div>
+            </li>
+
             {modules
                 .filter((module) => module.course === courseId)
                 .map((module, index) => (
                     <li key={index} className="list-group-item">
+                        <button
+                            className="btn green mx-3"
+                            onClick={() => dispatch(setModule(module))}
+                        >
+                            Edit
+                        </button>
+
+                        <button
+                            className="btn red mx-3"
+                            onClick={() => dispatch(deleteModule(module._id))}
+                        >
+                            Delete
+                        </button>
+
                         <h5>
                             {icons[0]} {module.name} -{" "}
                             <span>{module.description}</span>
--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
+import { React, useState } from "react";
 import db from "../Database";
 import "./index.css";
 
-function Dashboard() {
-    const courses = db.courses;
+function Dashboard({
+    courses,
+    course,
+    setCourse,
+    addNewCourse,
+    deleteCourse,
+    updateCourse,
+}) {
     return (
         <div class="dashboard-container">
             <div class="row mx-3">
@@ -17,6 +24,44 @@ function Dashboard() {
                 </div>
             </div>
             <div class="row mx-3">
+                <h5>Course</h5>
+                <input
+                    value={course.name}
+                    className="form-control w-25 mb-1"
+                    onChange={(e) =>
+                        setCourse({ ...course, name: e.target.value })
+                    }
+                />
+                <input
+                    value={course.number}
+                    className="form-control w-25 mb-1"
+                    onChange={(e) =>
+                        setCourse({ ...course, number: e.target.value })
+                    }
+                />
+                <input
+                    value={course.startDate}
+                    className="form-control w-25 mb-1"
+                    type="date"
+                    onChange={(e) =>
+                        setCourse({ ...course, startDate: e.target.value })
+                    }
+                />
+                <input
+                    value={course.endDate}
+                    className="form-control w-25 mb-1"
+                    type="date"
+                    onChange={(e) =>
+                        setCourse({ ...course, endDate: e.target.value })
+                    }
+                />
+                <button className="btn mx-2 mb-3" onClick={addNewCourse}>
+                    Add
+                </button>
+                <button className="btn mb-3" onClick={updateCourse}>
+                    Update
+                </button>
+
                 <div class="row row-cols-1 mx-3">
                     {courses.map((course) => (
                         <div class="col card-container">
@@ -41,6 +86,25 @@ function Dashboard() {
                                             202410_1 Spring 2023 Semester Full
                                             Term
                                         </p>
+                                        <button
+                                            className="btn mx-3 mb-2"
+                                            onClick={(event) => {
+                                                event.preventDefault();
+                                                setCourse(course);
+                                            }}
+                                        >
+                                            Edit
+                                        </button>
+
+                                        <button
+                                            className="btn mx-3"
+                                            onClick={(event) => {
+                                                event.preventDefault();
+                                                deleteCourse(course._id);
+                                            }}
+                                        >
+                                            Delete
+                                        </button>
                                     </div>
                                 </div>
                             </Link>
